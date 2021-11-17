f04e82a6de9739cce35352fe501d20f35012ac8c
sy-records <52o@qq52o.cn>
2021-11-17 16:34:13 +0800

    Add Hook Type

---
+++ src/Constants/Type.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of WebHooks.
+ *
+ * @link     https://github.com/sy-records/webhooks
+ * @contact  Lu Fei <i@lufei.dev>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+namespace Luffy\WebHook\Constants;
+
+class Type
+{
+    public const GITHUB_PUSH = 'push';
+
+    public const GITHUB_TAG_PUSH = 'push';
+
+    public const GITEE_PUSH = 'push_hooks';
+
+    public const GITEE_TAG_PUSH = 'tag_push_hooks';
+
+    public const GITLAB_PUSH = 'push';
+
+    public const GITLAB_TAG_PUSH = 'tag_push';
+}
