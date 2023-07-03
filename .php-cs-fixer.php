b374536872cdab8844fdb4f74ef30bd52b8d3952
sy-records <52o@qq52o.cn>
2023-07-03 08:50:13 +0800

    Change email

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
  * This file is part of WebHooks.
  *
  * @link     https://github.com/sy-records/webhooks
- * @contact  Lu Fei <i@lufei.dev>
+ * @contact  Lu Fei <lufei@simps.io>
  *
  * For the full copyright and license information,
  * please view the LICENSE file that was distributed with this source code.
@@ -14,7 +14,7 @@ $header = <<<'TEXT'
 This file is part of WebHooks.
 
 @link     https://github.com/sy-records/webhooks
-@contact  Lu Fei <i@lufei.dev>
+@contact  Lu Fei <lufei@simps.io>
 
 For the full copyright and license information,
 please view the LICENSE file that was distributed with this source code.
--- src/Constants/Header.php
+++ src/Constants/Header.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
  * This file is part of WebHooks.
  *
  * @link     https://github.com/sy-records/webhooks
- * @contact  Lu Fei <i@lufei.dev>
+ * @contact  Lu Fei <lufei@simps.io>
  *
  * For the full copyright and license information,
  * please view the LICENSE file that was distributed with this source code.
